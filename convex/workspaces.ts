81dd79672f02daa3b4f462d04f876b16ca99047c
apophis30 <aditya27may@apophis30.>
2025-01-08 09:59:05 +0000

    added toolbar components

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,4 +1,4 @@
-import { v } from "convex/values";
+import { convexToJson, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
 
@@ -30,5 +30,16 @@ export const get = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("workspaces").collect();
+  },
+});
+
+export const getById = query({
+  args: { id: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+    return await ctx.db.get(args.id);
   },
 });

15bf1535765a0e9712de454883bb1a31aa09189b
apophis30 <aditya27may@apophis30.>
2025-01-08 08:09:35 +0000

    added workspace schema and components

--- convex/schema.ts
+++ convex/schema.ts
@@ -3,7 +3,12 @@ import { authTables } from "@convex-dev/auth/server";
 import { defineSchema, defineTable } from "convex/server";
 
 const schema = defineSchema({
-    ...authTables,
+  ...authTables,
+  workspaces: defineTable({
+    name: v.string(),
+    userId: v.id("users"),
+    joinCode: v.string(),
+  }),
 });
 
 export default schema;
--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useAuthActions } from "@convex-dev/auth/react";
-import { useCurrentUser } from "@/features/auth/hooks/use-current-user";
+import { useCurrentUser } from "@/features/auth/api/use-current-user";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {

853d3eda3e0cc20a0b8d5170ad17f58e3872eb75
apophis30 <aditya27may@apophis30.>
2025-01-14 15:12:50 +0000

    Added workspace invite modal and change join code functionality

---
+++ src/app/workspace/[workspaceId]/invite-modal.tsx
@@ -0,0 +1,97 @@
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogClose,
+} from "@/components/ui/dialog";
+import { CopyIcon, RefreshCcw } from "lucide-react";
+
+import { useConfirm } from "@/hooks/use-confirm";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useNewJoinCode } from "@/features/workspaces/api/use-new-join-code";
+
+interface InviteModalProps {
+  open: boolean;
+  setOpen: (value: boolean) => void;
+  name: string;
+  joinCode: string;
+}
+
+export const InviteModal = ({
+  open,
+  setOpen,
+  name,
+  joinCode,
+}: InviteModalProps) => {
+  const workspaceId = useWorkspaceId();
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are you sure?",
+    "This will deactivate current invite code and generate a new code."
+  );
+
+  const { mutate, isPending } = useNewJoinCode();
+
+  const handleNewCode = async () => {
+    const ok = await confirm();
+    if (!ok) return;
+
+    mutate(
+      { workspaceId },
+      {
+        onSuccess: () => {
+          toast.success("Invite code generated");
+        },
+        onError: () => {
+          toast.error("Failed to regenerate invite code");
+        },
+      }
+    );
+  };
+
+  const handleCopy = () => {
+    const inviteLink = `${window.location.origin}/join/${workspaceId}`;
+    navigator.clipboard
+      .writeText(inviteLink)
+      .then(() => toast.success("Invite link copieed to clipboard"));
+  };
+
+  return (
+    <>
+      <ConfirmDialog />
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Invite people to your {name}</DialogTitle>
+            <DialogDescription>
+              Use code below to invite people to your workspace
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col gap-y-4 items-center justify-center py-10">
+            <p className="text-4xl font-bold tracking-widest">{joinCode}</p>
+            <Button onClick={handleCopy} variant={"ghost"} size={"sm"}>
+              Copy Code
+              <CopyIcon className="size-4 ml-2" />
+            </Button>
+          </div>
+          <div className="flex items-center justify-between w-full">
+            <Button
+              disabled={isPending}
+              onClick={handleNewCode}
+              variant={"outline"}
+            >
+              New Code
+              <RefreshCcw className="size-4 ml-2" />
+            </Button>
+            <DialogClose asChild>
+              <Button>Close</Button>
+            </DialogClose>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -4,13 +4,13 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
 import { useState } from "react";
 
+import { InviteModal } from "@/app/workspace/[workspaceId]/invite-modal";
 import { PreferencesModal } from "@/app/workspace/[workspaceId]/preferences-modal";
 import { Hint } from "@/components/hint";
 import { Doc } from "../../../../convex/_generated/dataModel";
@@ -25,9 +25,16 @@ export const WorkspaceHeader = ({
   isAdmin,
 }: WorkspaceHeaderProps) => {
   const [preferencesOpen, setPreferencesOpen] = useState(false);
+  const [inviteOpen, setInviteOpen] = useState(false);
 
   return (
     <>
+      <InviteModal
+        open={inviteOpen}
+        setOpen={setInviteOpen}
+        name={workspace.name}
+        joinCode={workspace.joinCode}
+      />
       <PreferencesModal
         open={preferencesOpen}
         setOpen={setPreferencesOpen}
@@ -62,7 +69,7 @@ export const WorkspaceHeader = ({
                 <DropdownMenuSeparator />
                 <DropdownMenuItem
                   className="cursor-pointer py-2"
-                  onClick={() => {}}
+                  onClick={() => setInviteOpen(true)}
                 >
                   Invite people to {workspace.name}
                 </DropdownMenuItem>
